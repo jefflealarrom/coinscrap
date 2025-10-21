c1c6b58fb61bf7c8f71930c22834b0382571c094
jefflealarrom <jefflealarrom@gmail.com>
2025-10-21 19:49:32 +0200

    TrabsactionList TDD - RED

---
+++ src/app/components/test/TransactionsList.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
+import TransactionsList from './TransactionList';
+
+const mockTransactions = [
+  {
+    id: 'tx_001',
+    date: '2025-08-02',
+    description: 'Supermercado DIA',
+    amount: -37.45,
+    category: 'Groceries',
+  },
+  {
+    id: 'tx_002',
+    date: '2025-08-03',
+    description: 'Nómina',
+    amount: 1450.0,
+    category: 'Income',
+  },
+  {
+    id: 'tx_003',
+    date: '2025-08-04',
+    description: 'Netflix',
+    amount: -12.99,
+    category: 'Subscriptions',
+  },
+];
+
+describe('TransactionsList', () => {
+  it('filtra por texto correctamente', async () => {
+    render(<TransactionsList items={mockTransactions} simulateLoadingMs={0} />);
+
+  const input = screen.getByLabelText(/buscar por descripción/i);
+    fireEvent.change(input, { target: { value: 'Netflix' } });
+  await waitForElementToBeRemoved(() => screen.queryByText(/Cargando transacciones.../i));
+
+  expect(await screen.findByText(/Netflix/)).toBeInTheDocument();
+  expect(screen.queryByText(/Supermercado DIA/)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Nómina/)).not.toBeInTheDocument();
+  });
+});
